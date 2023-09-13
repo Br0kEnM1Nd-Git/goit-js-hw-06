03e2bc025f6ebad8b712387ad2125bc6595117aa
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-09-14 02:39:15 +0300

    task8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", subFunc);
+function subFunc(event) {
+  event.preventDefault();
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (email === "" || password === "") {
+    alert("Заповніть усі поля");
+  } else {
+    const userData = {
+      email,
+      password,
+    };
+    console.log(userData);
+    form.reset();
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+const btnChange = document.querySelector(".change-color");
+const spColor = document.querySelector(".color");
+btnChange.addEventListener("click", changeColor);
+function changeColor() {
+  const ranColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = `${ranColor}`;
+  spColor.textContent = `${ranColor}`;
+}
