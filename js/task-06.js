716b0103c150db6c7745b33f71b796ed3ad9ad80
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-09-14 04:11:49 +0300

    task6upgrade

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const maxLength = parseInt(validationInput.dataset.length);
 validationInput.addEventListener("blur", validate);
 function validate(event) {
   const curLength = event.currentTarget.value.length;
-  if (curLength > maxLength) {
+  if (curLength > maxLength || curLength <= 0) {
     validationInput.setAttribute("class", "invalid");
   } else {
     validationInput.setAttribute("class", "valid");
